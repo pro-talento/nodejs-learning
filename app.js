774a5661feae0b1a62e4402d592d252427e94fd4
Misael Calvillo Mancilla <misa@homely.mx>
2023-01-17 19:58:20 -0600

    Install 'dotenv' and 'express'

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log('Programa inicializado ✅');
+require('dotenv').config()
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor inicializado en puerto ${PORT} 🚀`);
+});
