2ec59871d1eb5ad16576bf6a9acae21a1cc05655
Misael Calvillo Mancilla <misa@careme.mx>
2023-02-21 19:09:52 -0600

    Agrega tareas pendientes

--- src/models/users.js
+++ src/models/users.js
@@ -1,14 +1,16 @@
 const db = require('./db')
 
 async function createUserModel(newUser) {
+  
+  // TODO: Cifrar contraseña
   const createdUser = await db.users.create(newUser)
-  //...
   return createdUser
 }
 
 async function loginUserModel(credentials) {
   const userToLog = await db.users.getByEmail(credentials.email)
 
+  // TODO: Cifrar contraseña
   if (userToLog.password !== credentials.password) {
     return null
   }
